cebbe5d8ba4cffe2b3895adf5c2ebc0e3d013d88
Himanshu Yadav <himanshuyadav1607@gmail.com>
2023-07-08 18:05:01 +0530

    first commit

---
+++ src/test/java/com/automationSelenium/testCases/IFrameTest.java
@@ -0,0 +1,22 @@
+package com.automationSelenium.testCases;
+
+import com.automationSelenium.BaseClass;
+import com.automationSelenium.pages.AlertPage;
+import com.automationSelenium.pages.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class IFrameTest extends BaseClass {
+
+    @Test
+    public void iFrameTest() throws InterruptedException {
+        WebDriver driver = getDriver();
+        HomePage homePage = new HomePage(driver);
+        homePage.frontPage();
+        homePage.clickSwitchAlert();
+       // homePage.iFrame();
+
+    }
+
+
+}
