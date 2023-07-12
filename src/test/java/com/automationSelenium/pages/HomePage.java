15bc6c7675a70794a72873c42ef9bcdd79c8709b
Himanshu Yadav <himanshuyadav1607@gmail.com>
2023-07-12 16:23:42 +0530

    second commit

--- src/test/java/com/automationSelenium/pages/HomePage.java
+++ src/test/java/com/automationSelenium/pages/HomePage.java
@@ -209,12 +209,6 @@ public class HomePage extends BasePage {
         Select menuSel = new Select(thirdFrameMenuFromFistFrame);
         menuSel.selectByVisibleText("Home");
 
-
-
-
-
-
-
-
+        
     }
 }
